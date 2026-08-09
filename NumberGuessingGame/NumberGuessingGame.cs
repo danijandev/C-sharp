56aac6165253eb14cc64fb298d2cfe5c52d5c117
danijandev <danijan.dev@gmail.com>
2026-08-09 19:27:47 +0800

    NumberGuessingGame

---
+++ NumberGuessingGame/NumberGuessingGame.cs
@@ -0,0 +1,37 @@
+using System;
+
+class NumberGuessingGame 
+{
+    static void Main(string[] args)
+    {
+        int targetNumber = Random.Shared.Next(1, 101);
+        int attempts = 1;
+        
+        while (true)
+        {
+            Console.Write($"Guess the number between 1-100 (Attempt {attempts}): ");
+            string guessInput = Console.ReadLine();
+            bool isInputValid = int.TryParse(guessInput, out int guess);
+            
+            if (isInputValid)
+            {
+                attempts++;
+                
+                if (targetNumber > guess)
+                {
+                    Console.WriteLine("The number is greater than your guess (Too low)!");
+                } else if (targetNumber < guess)
+                {
+                    Console.WriteLine("The number is less than your guess (Too high)!");
+                } else
+                {
+                    Console.WriteLine($"You guessed correct! Total attempts: {attempts}");
+                    break;
+                }
+            } else 
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
---
+++ NumberGuessingGame/README.md
@@ -0,0 +1,13 @@
+# NumberGuessingGame
+This is a C# practice project by Danijan. Created on August 9, 2026.
+
+## Description
+Guess a number between 1 and 100. You'll be told if your guess is too high or too low after each attempt.
+
+## Requirements
+- [.NET SDK](https://dotnet.microsoft.com/download)
+
+## Run
+```bash
+dotnet run
+```
